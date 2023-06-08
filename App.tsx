0fff0a64a86511578867b5480a5c797057c3d28e
okataku <t.okada.dev@gmail.com>
2023-06-08 19:45:26 +0900

    First commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import { useMemo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { isClip } from 'react-native-app-clip'
 
 export default function App() {
+  const isAppClip = useMemo(() => isClip(), []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{isAppClip ? 'App Clip' : 'Main App'}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ docs/index.md
@@ -0,0 +1,11 @@
+# App Clip
+
+- [App Clipの配信](https://developer.apple.com/jp/documentation/app_clips/distributing_your_app_clip/)
+- [App Clipの起動体験のテスト](https://developer.apple.com/jp/documentation/app_clips/testing_your_app_clip_s_launch_experience/)
+
+## EAS ビルドエラー
+
+![eas-build-error](./images/eas-build-error.png)
+
+XCodeでプロジェクトの設定などを調整する必要がありそう（[参考](https://zenn.dev/yahiro/scraps/3cf3e728a258aa)）。  
+react-native-app-clip の AppConfig でいろいろとプロジェクトの設定などを変更していると思うのでそこに問題があるのかもしれない。
